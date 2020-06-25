87fd37b076aa5211ba8d4a6d5231dcbd70ebadd9
Pedro Marques <pmarques93@gmail.com>
2020-06-25 03:59:30 +0100

    Add boss spawner anim

---
+++ FantasticGame/Assets/Scripts/Enemies/Enemies/BossSpawner.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+sealed public class BossSpawner : MonoBehaviour
+{
+
+    [SerializeField] private GameObject smokeSpawner;
+
+    public void DestroyMe()
+    {
+        Instantiate(smokeSpawner, transform.position, transform.rotation);
+        Destroy(gameObject);
+    }
+}
